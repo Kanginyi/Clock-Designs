d669385115237ad1306134279c905a5fd1c45276
Eric Yi <erickanginyi@gmail.com>
2021-12-17 14:57:26 -0500

    Add function for ticking clock hands

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
- const hour = document.querySelector("hour");
- const minute = document.querySelector("minute");
- const second = document.querySelector("second");
+ const hour = document.querySelector("#hour");
+ const minute = document.querySelector("#minute");
+ const second = document.querySelector("#second");
 
 let set_clock = setInterval(() => {
     const date = new Date();
@@ -9,5 +9,11 @@ let set_clock = setInterval(() => {
     const dateMinute = date.getMinutes();
     const dateSecond = date.getSeconds();
 
-    console.log(dateHour, dateMinute, dateSecond)
+    let hourTick = (dateHour * 30) + (dateMinute / 2);
+    let minTick = (dateMinute * 6) + (dateSecond / 10);
+    let secTick = dateSecond * 6;
+
+    hour.style.transform = `rotate(${hourTick}deg)`;
+    minute.style.transform = `rotate(${minTick}deg)`;
+    second.style.transform = `rotate(${secTick}deg)`;
 }, 1000);
